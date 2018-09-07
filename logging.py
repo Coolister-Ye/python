aa6264c4381ea8da3509ede7889fd87ee449f3f7
JasonYee <34277798+JasonYee@users.noreply.github.com>
2018-09-07 14:15:32 +0800

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,38 @@
+"""
+This file record the common usage of logging.
+Level: DEBUG, INFO, WARMING, ERROR, CRITIAL
+Logging method: info(), debug(), warn(), warning, error, exception, critical
+"""
+
+import logging
+
+# simple logging with error level
+logging.error("This is the error message!!!")
+
+# logging to a file
+logging.basicConfig(filename='logs.log', level=logging.DEBUG)
+logging.info("This is the info message!!!")
+
+# logging to a file with overwrite mode
+logging.basicConfig(filename='logs.log', filemode='w', level=logging.DEBUG)
+logging.info("This is the info messgae!!!")
+
+# logging from multiple modules
+# main.py
+import logging
+import mymodule
+
+def main():
+    logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    logging.info('Started')
+    mylib.do_something()
+    logging.info('Finished')
+
+if __name__ == '__main__':
+    main()
+
+# mymoduler.py
+import logging
+
+def do_something():
+    logging.info('Doing something')
